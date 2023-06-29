7271647a42a0137b5b79e6296a447dffdd10c54a
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-06-28 21:40:44 -0500

    Post Entity

--- src/post/entities/post.entities.ts
+++ src/post/entities/post.entities.ts
@@ -1,1 +1,16 @@
-export class PostEntities {}
+import { Comments } from 'src/comments/entities/comments.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('post')
+export class PostEntities {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 15 })
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @OneToMany(()=> Comments, (comment) => comment.post)
+}
